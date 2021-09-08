71a2435a57b343b7ed804d7b3b951c64ac1c8b35
jhorman <jhorman@gmail.com>
2021-09-08 13:03:12 -0700

    Use json logger

--- serve.py
+++ serve.py
@@ -17,6 +17,7 @@ from twisted.web.server import Site, NOT_DONE_YET, GzipEncoderFactory
 from twisted.web.static import File
 
 import gentle
+import logconfig
 from gentle.util.cyst import Insist
 from gentle.util.paths import get_resource, get_datadir
 
@@ -403,6 +404,7 @@ if __name__=='__main__':
 
     args = parser.parse_args()
 
+    logconfig.configure_logging()
     log_level = args.log.upper()
     logging.getLogger().setLevel(log_level)
 
